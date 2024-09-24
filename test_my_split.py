9e1adb34d0efe722f937a3f2f2c75a249eb75b6f
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-24 14:29:05 -0400

    write test cases for my_split()

--- test_my_split.py
+++ test_my_split.py
@@ -5,3 +5,51 @@ Created: September 2024
 Developer: Swathi Danturi
 Date: 9/23/2024
 """
+
+import pytest
+
+from sentence import Sentence
+
+def test_my_split_chars_digits():
+    """
+    Test sentence that has characters and digits
+    """
+    test_string = 'COMP 801.M1 ICP Fall 2024!!'
+    sentence_obj = Sentence(test_string)
+    expected = ['COMP','801.M1','ICP','Fall','2024!!']
+    sentence_obj.my_split()
+    actual = sentence_obj.words
+    assert actual == expected
+
+def test_my_split_extra_spaces():
+    """
+    Test sentence that has extra spaces
+    """
+    test_string = '  Split function   returns a list       '
+    sentence_obj = Sentence(test_string)
+    expected = ['Split','function','returns','a','list']
+    sentence_obj.my_split()
+    actual = sentence_obj.words
+    assert actual == expected
+
+def test_my_split_one_word():
+    """
+    Test sentence that has only one word
+    """
+    test_string = 'Oneword'
+    sentence_obj = Sentence(test_string)
+    expected = ['Oneword']
+    sentence_obj.my_split()
+    actual = sentence_obj.words
+    assert actual == expected
+
+def test_my_split_empty_string():
+    """
+    Test sentence that is an empty string
+    """
+    test_string = ''
+    sentence_obj = Sentence(test_string)
+    expected = ['']
+    sentence_obj.my_split()
+    actual = sentence_obj.words
+    assert actual == expected
