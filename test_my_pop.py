a2bb90af6028c9d3c42786d6ee9cfc5039b5c4c6
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-24 19:12:51 -0400

    write test cases for my_pop()

--- test_my_pop.py
+++ test_my_pop.py
@@ -21,9 +21,27 @@ def test_my_pop_legal_index():
 
 def test_my_pop_not_legal_index():
     """
-    Test string with an invalid index
+    Test string with a not valid index
     """
     sentence_obj = Sentence('Make well use of the career fair')
-    actual = sentence_obj.my_pop(9)
+    actual = sentence_obj.my_pop(13)
+    expected = None
+    assert actual == expected
+
+def test_my_pop_legal_negative_index():
+    """
+    Test string with a valid negative index
+    """
+    sentence_obj = Sentence('Due Date for Assignment is September 25')
+    actual = sentence_obj.my_pop(-4)
+    expected = 'Assignment'
+    assert actual == expected
+
+def test_my_pop_not_legal_negative_index():
+    """
+    Test string with an invalid negative index
+    """
+    sentence_obj = Sentence('Make well use of the career fair')
+    actual = sentence_obj.my_pop(-9)
     expected = None
     assert actual == expected
