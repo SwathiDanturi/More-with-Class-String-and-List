7b19acc83c2eb591722c7026015f9408852c6451
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-24 17:04:24 -0400

    write test cases for my_join()

--- test_my_join.py
+++ test_my_join.py
@@ -5,3 +5,43 @@ Created: September 2024
 Developer: Swathi Danturi
 Date: 9/23/2024
 """
+
+import pytest
+
+from sentence import Sentence
+
+def test_my_join_chars_digits():
+    """
+    Test sentence that has characters and digits
+    """
+    sentence_obj = Sentence('Branch is ahead of 4 commits')
+    expected = 'Branch is ahead of 4 commits'
+    actual = sentence_obj.my_join()
+    assert actual == expected
+
+def test_my_join_extra_spaces():
+    """
+    Test sentence that has extra spaces
+    """
+    sentence_obj = Sentence(' my_join method    implementaion   ')
+    expected = 'my_join method implementation'
+    actual = sentence_obj.my_join()
+    assert actual == expected
+
+def test_my_join_one_word():
+    """
+    Test sentence that has only one word
+    """
+    sentence_obj = Sentence('A_Word')
+    expected = 'A_Word'
+    actual = sentence_obj.my_join()
+    assert actual == expected
+
+def test_my_join_empty_string():
+    """
+    Test sentence that is an empty string
+    """
+    sentence_obj = Sentence('')
+    expected = ''
+    actual = sentence_obj.my_join()
+    assert actual == expected
