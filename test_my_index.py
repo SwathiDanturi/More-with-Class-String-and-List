970026a83c45b676c7ccea50d9e728859de4276c
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-24 17:39:05 -0400

    write test cases for my_index()

--- test_my_index.py
+++ test_my_index.py
@@ -5,3 +5,25 @@ Created: September 2024
 Developer: Swathi Danturi
 Date: 9/23/2024
 """
+
+import pytest
+
+from sentence import Sentence
+
+def test_my_index_word_found():
+    """
+    Test sentence that contains the word to be searched for
+    """
+    sentence_obj = Sentence('University of New Hampshire, Manchester')
+    actual = sentence_obj.my_index('New')
+    expected = 2
+    assert actual == expected
+
+def test_my_index_word_notfound():
+    """
+    Test sentence that does not contain the word to be searched for
+    """
+    sentence_obj = Sentence('Prepare well for the exam')
+    actual = sentence_obj.my_index('New')
+    expected = None
+    assert actual == expected
